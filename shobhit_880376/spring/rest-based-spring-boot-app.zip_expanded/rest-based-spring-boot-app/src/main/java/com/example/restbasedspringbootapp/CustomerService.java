1b46aa69b28cdca440de3df7cc29655283270786
shobhit242 <shobhitss28@gmail.com>
2020-12-09 10:03:52 +0530

    spring demo

---
+++ shobhit_880376/spring/rest-based-spring-boot-app.zip_expanded/rest-based-spring-boot-app/src/main/java/com/example/restbasedspringbootapp/CustomerService.java
@@ -0,0 +1,40 @@
+package com.example.restbasedspringbootapp;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+	@Autowired
+	private CustomerRepository dao;
+
+	public Customer addCustomer(Customer customer) {
+		Customer createdCustomer = dao.save(customer);
+		return createdCustomer;
+	}
+	public List<Customer> fetchCustomers(){
+		return dao.findAll();
+	}
+	public Customer fetchCustomer(int id) {
+		Optional<Customer> option = dao.findById(id);
+		if(option.isPresent()) 
+			return option.get();
+		else 
+			return null;
+	}
+	public Customer updateCustomer(int id, LocalDate dob) {
+		Customer customer = fetchCustomer(id);
+		if(customer != null) {
+			customer.setDob(dob);
+			return dao.save(customer);
+		} 
+		else 
+			return null;
+	}
+}
